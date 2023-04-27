2d891eb191841aab10458accef7eb3db0eb9cced
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-04-27 16:30:23 +0300

    add functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,54 @@
+import { useState, useCallback } from 'react';
+import calculate from '../logic/calculate';
 import './style.css';
 
 function Calculator() {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+  const clickHandler = useCallback((e) => {
+    const results = calculate(state, e.target.innerText);
+    setState(results);
+  }, [state]);
+  const { total, next, operation } = state;
   return (
     <div className="calc">
       <div className="calcWrapper">
+        <p>
+          {total}
+          {' '}
+          {operation}
+          {' '}
+          {next}
 
+        </p>
         <div className="buttonWrapper">
-          <p>0</p>
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="sign">/</button>
 
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="sign">x</button>
+          <button onClick={clickHandler} type="button">AC</button>
+          <button onClick={clickHandler} type="button">+/-</button>
+          <button onClick={clickHandler} type="button">%</button>
+          <button onClick={clickHandler} type="button" className="sign">÷</button>
 
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="sign">-</button>
+          <button onClick={clickHandler} type="button">7</button>
+          <button onClick={clickHandler} type="button">8</button>
+          <button onClick={clickHandler} type="button">9</button>
+          <button onClick={clickHandler} type="button" className="sign">x</button>
 
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="sign">+</button>
+          <button onClick={clickHandler} type="button">4</button>
+          <button onClick={clickHandler} type="button">5</button>
+          <button onClick={clickHandler} type="button">6</button>
+          <button onClick={clickHandler} type="button" className="sign">-</button>
 
-          <button type="button" className="zero">0</button>
-          <button type="button">.</button>
-          <button type="button" className="sign">=</button>
+          <button onClick={clickHandler} type="button">1</button>
+          <button onClick={clickHandler} type="button">2</button>
+          <button onClick={clickHandler} type="button">3</button>
+          <button onClick={clickHandler} type="button" className="sign">+</button>
+
+          <button onClick={clickHandler} type="button" className="zero">0</button>
+          <button onClick={clickHandler} type="button">.</button>
+          <button onClick={clickHandler} type="button" className="sign">=</button>
 
         </div>
       </div>
