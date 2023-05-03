7704d01e5ed529fa52ae80b711467cd123de3d29
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-05-03 15:04:12 +0300

    solve linter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import CalculatorPage from './pages/Calculator';
-import Home from './pages/Home';
-import Navbar from './nav';
-import QoutePage from './pages/Qoute';
+import Calculator from './components/Calculator';
+import Qoute from './components/qoute';
 
 function App() {
   return (
 
     <div className="App">
-      <>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/Home" element={<Home />} />
-            <Route path="/calculator" element={<CalculatorPage />} />
-            <Route path="/qoute" element={<QoutePage />} />
-          </Routes>
-        </div>
-      </>
+      <div className="qoute_wrapper">
+        <Qoute />
+      </div>
 
+      <div className="calculator_wrapper">
+        <Calculator />
+      </div>
     </div>
   );
 }
