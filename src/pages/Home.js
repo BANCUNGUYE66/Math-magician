ea9b5dfbeb8522e7184c8c788c0fb843753c8ae4
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-05-03 18:50:59 +0300

    add pages

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './style.css';
+
+const Home = () => (
+  <div className="home_container">
+    <h1>
+      Welcome To Our Page!
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Sed sed suscipit diam. Vestibulum ante ipsum primis in
+      faucibus orci luctus et ultrices posuere cubilia curae;
+      Curabitur risus leo, hendrerit eget ornare in, tempus non
+      sapien. Nulla luctus sapien a dui pretium, vitae gravida
+
+      <br />
+      <br />
+      ipsum fermentum. Vestibulum ultrices tempus aliquam.
+      Donec eros lectus, congue vel mi vel, bibendum convallis
+      metus. Proin id enim eget elit vulputate dignissim ultrices
+      sed sem. In vestibulum ipsum nunc, in molestie purus tempus
+      eu. Vivamus fringilla massa ut ex egestas commodo ut sed quam.
+
+    </p>
+
+  </div>
+);
+
+export default Home;
